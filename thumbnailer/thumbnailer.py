783442cd362b742a8dc18c7df38f7d5a87ff0ca0
Trist <tristantrim@gmail.com>
2013-07-23 19:26:00 -0300

    url is now to be called from settings.
    filetype checking if statements now call pre defined lists of possible filetypes.
    
    thumbnail now calls to seperate functions to make the preview images for jsc3d and browser rendering.
    
    url, media_root_path, and media_url_path are now variables defined at the begining of each render function. They improve clarity and stop the program from making so many calls.

--- thumbnailer/thumbnailer.py
+++ thumbnailer/thumbnailer.py
@@ -12,54 +12,78 @@ import subprocess
 #Relative path to whatever I sent you. So fielpath, but relative to the web server. We're going to do this multi-threaded, so I can't gurantee I get data back in the order I put it in.
 #What to render it with. We don't have all the renderes and what files to use sorted out yet, but for stuff that can just be rendered in the browser, like images, "browser" is the rendered. Other file types will have custom javascript renderers.
 
-def thumbnail(filepath,size):
-	extension = os.path.splitext(filepath)[1]
-	str_thumbnail_size = "-"+str(size[0])+"X"+str(size[1])
 
-	URL="http://testing.monastery0.org"
+def jsc3d_render(filepath,size):
 
-	if extension == ".stl" or extension == ".obj" or extension ==  ".STL" or extension ==  ".OBJ":
-		#checks to see ig the thumbnail already exists
-		if os.path.exists(settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png"):
-			return(settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"jsc3d")
-		else:		
-			builddir(settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH))
-			#Uses phantomjs to create thumbnails.
-			subprocess.call(os.path.realpath(os.path.dirname(__file__)) + "/phantom/bin/phantomjs" +" "+
-				str(os.path.realpath(os.path.dirname(__file__)) + "/phantom/rastersize.js"+" "+
-				"\""+URL+"/thumbs/stl/"+os.path.relpath(filepath,settings.PROJECT_PATH))+"\""+" "+
-				"\""+settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png"+"\""+" "+
-				str(size[0])+ " " + str(size[1])
-				, shell=True
-			)
-			return(settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"jsc3d")
+        URL=settings.URL
+        media_root_path = settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)
+        media_url_path = settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)
 
-	elif extension == ".png" or extension == ".jpg" or extension == ".gif":
-		 #checks to see ig the thumbnail already exists
-		if os.path.exists(settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png"):
-			return(settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"browser")
-		else:
-			img = Image.open(filepath)
-			img.thumbnail(size)
-			builddir(settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH))
-			img.save(settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png", "PNG")
-			return(settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"browser")
+
+        #checks to see ig the thumbnail already exists
+        if os.path.exists(media_root_path+str_thumbnail_size+".png"):
+                return(media_url_path+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"jsc3d")
+        else:
+                builddir(media_root_path)
+                #Uses phantomjs to create thumbnails.
+                subprocess.call(os.path.realpath(os.path.dirname(__file__)) + "/phantom/bin/phantomjs" +" "+
+                        str(os.path.realpath(os.path.dirname(__file__)) + "/phantom/rastersize.js"+" "+
+                        "\""+URL+"/thumbs/stl/"+os.path.relpath(filepath,settings.PROJECT_PATH))+"\""+" "+
+                        "\""+media_root_path+str_thumbnail_size+".png"+"\""+" "+
+                        str(size[0])+ " " + str(size[1])
+                        , shell=True
+                )
+                return(media_url_path+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"jsc3d")
+
+
+def browser_render(filepath,size):
+
+        URL=settings.URL
+        media_root_path = settings.MEDIA_ROOT+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)
+        media_url_path = settings.MEDIA_URL+"thumbnails/"+os.path.relpath(filepath,settings.PROJECT_PATH)
+
+
+        #checks to see ig the thumbnail already exists
+        if os.path.exists(media_root_path+str_thumbnail_size+".png"):
+                return(media_url_path+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"browser")
+        else:
+                img = Image.open(filepath)
+                img.thumbnail(size)
+                builddir(media_root_path)
+                img.save(media_root_path+str_thumbnail_size+".png", "PNG")
+                return(media_url_path+str_thumbnail_size+".png","/"+os.path.relpath(filepath,settings.PROJECT_PATH),"browser")
 
 
 def builddir(outpath):
-	d = os.path.dirname(outpath)
-	if not os.path.exists(d):
-		os.makedirs(d)
-
+        d = os.path.dirname(outpath)
+        if not os.path.exists(d):
+                os.makedirs(d)
 
 
 def genericthumb(filepath,size):
-	str_thumbnail_size = "-"+str(size[0])+"X"+str(size[1])
+        str_thumbnail_size = "-"+str(size[0])+"X"+str(size[1])
 
-	img = Image.open(settings.MEDIA_ROOT+"images/genericthumb.png")
-	img.thumbnail(size)
-	img.save(settings.MEDIA_ROOT+"thumbnails/genericthumb"+str_thumbnail_size+".png")
-	return(settings.MEDIA_URL+"thumbnails/genericthumb"+str_thumbnail_size+".png",filepath,"norender")
-	logger.warning('not a supported filetype: ' + "\"" + filepath + "\"")
+        img = Image.open(settings.MEDIA_ROOT+"images/genericthumb.png")
+        img.thumbnail(size)
+        img.save(settings.MEDIA_ROOT+"thumbnails/genericthumb"+str_thumbnail_size+".png")
+        return(settings.MEDIA_URL+"thumbnails/genericthumb"+str_thumbnail_size+".png",filepath,"norender")
+        logger.warning('not a supported filetype: ' + "\"" + filepath + "\"")
+
+
+
+def thumbnail(filepath,size):
+        
+        extension = os.path.splitext(filepath)[1]
+        str_thumbnail_size = "-"+str(size[0])+"X"+str(size[1])
+
+        jsc3d_pic=[".stl",".obj",".STL",".OBJ"]
+        browser_pic=[ ".png",".jpg",".gif"]
+
+        if extension in jsc3d_pic:
+                jsc3d_render(filepath,size)
+
+
+        elif extension in browser_pic:
+                browser_render(filepath,size)
 
 
