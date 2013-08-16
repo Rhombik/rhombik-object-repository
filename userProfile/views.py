011e32f8c34e322fef7fcfd00e6ff2961a381bdd
Trist <tristantrim@gmail.com>
2013-08-16 08:50:23 -0300

    "everything is broken and I don't know what it means!"

--- userProfile/views.py
+++ userProfile/views.py
@@ -1,5 +1,17 @@
 
+from django.shortcuts import render
+from django.template import RequestContext, loader
 
-# Create your views here.
+from django.contrib.auth.models import User
+from userProfile.models import userProfile
+
+
+def index(request, user):
+
+    """bleh blebh bhel bleh, IM GOING INSANE.... I mean; user profile display stuff."""
+    userdata=User.objects.filter(username=user).get()
+    help(userdata.userProfile.profilePicPath)
+    c = RequestContext(request, dict(userPic=userdata.userProfile.profilePicPath, user=request.user, bio=userProfile.bio))
+    return render(request, "article.html", c)
 
 
