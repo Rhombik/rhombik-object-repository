6f196d2b65505b59506d888de234002757010c7f
Trist <tristantrim@gmail.com>
2013-08-16 11:27:07 -0300

    ''This is a somewhat nearly functional user profile page.'' ''There are two problems though; It's ugly, and the users posts are actually just all the posts''

--- userProfile/views.py
+++ userProfile/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 
+from post.models import Post
 from django.contrib.auth.models import User
 from userProfile.models import userProfile
 
@@ -10,8 +11,12 @@ def index(request, user):
     
     """bleh blebh bhel bleh, IM GOING INSANE.... I mean; user profile display stuff."""
     userdata=User.objects.filter(username=user).get()
-    print(userdata.get_profile().profilePicPath)
-    c = RequestContext(request, dict(userPic=userdata.userProfile.profilePicPath, user=request.user, bio=userProfile.bio))
-    return render(request, "article.html", c)
+    #help(Post.objects.filter)
+    #print(user)
+    userposts=Post.objects.all()#filter(author=user).get() '''~this needs to get the users posts.... not just you know, all the posts....'''
+    #help(userposts)
+    '''the correct answer was "print(userdata.get_profile().profilePicPath)"   '''
+    c = RequestContext(request, dict(userPic=userdata.get_profile().profilePicPath, user=user, bio=userdata.get_profile().bio, userposts = userposts))
+    return render(request, "userProfile/index.html", c)
 
 
