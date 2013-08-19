19e476ce45a629a0e74437a488a88f9ad98f4eb3
traverseda <traverse.da@gmail.com>
2013-08-19 17:22:03 -0300

    Copied /register/ to be used as a user editor

--- userProfile/views.py
+++ userProfile/views.py
@@ -6,6 +6,37 @@ from post.models import Post
 from django.contrib.auth.models import User
 from userProfile.models import userProfile
 
+#this obviously doesn't work... But it's a good base to work from.
+@csrf_exempt
+def register(request):
+    form = registerForm()
+    if request.method == 'POST':
+        form = registerForm(request.POST)
+        profileform = UserProfileForm(request.POST)
+        if form.is_valid() and profileform.is_valid():
+            data = User();
+            data.username = form.cleaned_data["username"]
+            #data.password = form.cleaned_data["password"]
+            data.set_password(form.cleaned_data["password"])
+            data.save()
+            profile = userProfile()
+            profile.user = data
+            profile.profilepic = "/"
+            profile.bio = profileform.cleaned_data["bio"]
+            profile.save
+            return render_to_response('register.html', dict( user=request.user, msg="success"))
+        else:
+            print(request.POST)
+            return render_to_response('register.html', dict( user=request.user, form=form, form2=profileform))#registerForm(request.POST)))#form, msg=form.errors))
+    else:
+        form = registerForm()
+        profileform = UserProfileForm()
+        return render_to_response('register.html', dict( user=request.user, form=form, form2=profileform))
+
+def logout_user(request):
+    logout(request)
+    return redirect("/login")
+
 
 def index(request, user):
     
