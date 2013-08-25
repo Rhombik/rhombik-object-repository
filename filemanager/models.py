85507432a73139833353fc83ae03260f6201d168
traverseda <traverse.da@gmail.com>
2013-08-25 15:44:40 -0300

    Stuff

--- filemanager/models.py
+++ filemanager/models.py
@@ -8,7 +8,7 @@ class filename(models.Model):
     post = models.ForeignKey(Post,)
     filename = models.FileField(upload_to="uploads/"+post.title)
     thumbnailpath = models.CharField(max_length=256, blank=True, null=True)
-    filetype = models.CharField(max_length=60, blank=True, null=True)
+    filetype = models.CharField(max_length=8, blank=True, null=True)
     def save(self):
         thumbnaildata = thumbnailer.thumbnailer.thumbnail(self.filename,(200,200), forceupdate=True)
         self.thumbnailpath = thumbnaildata[0]
