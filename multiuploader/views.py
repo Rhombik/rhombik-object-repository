c6a6cf2a48fbd330b5cb8147962c795e54571fb9
traverseda <traverse.da@gmail.com>
2013-08-26 02:14:27 -0300

    Fixed annoyting bullshit.

--- multiuploader/views.py
+++ multiuploader/views.py
@@ -24,7 +24,7 @@ log = logging
 
 from multiuploader.forms import MultiuploaderImage
 from post.models import *
-from filemanage.models import fileobject
+from filemanager.models import fileobject
 
 
 @csrf_exempt
