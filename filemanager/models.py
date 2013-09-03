549a6736629da9cc8d5565d0d3ba3c7e4852be6a
traverseda <traverse.da@gmail.com>
2013-09-03 07:03:40 -0300

    Edited filemanager.models.fileobject to work with the *new* theoretical thumbnailser.

--- filemanager/models.py
+++ filemanager/models.py
@@ -13,24 +13,25 @@ class fileobject(models.Model):
     post = models.ForeignKey(Post)
     subfolder = models.CharField(max_length=256, default="/")
     filename = models.FileField(upload_to=uploadpath)
-    previewurl = models.CharField(max_length=256, blank=True, null=True)
-    thumbnailpath = models.CharField(max_length=256, blank=True, null=True)
+    thumbname = models.FileField(upload_to="/thumbs/", blank=True, null=True)
     filetype = models.CharField(max_length=60, blank=True, null=True)
 
 
     def save(self):
         super(fileobject, self).save()
         try:
-            thumbnaildata = thumbnailer.thumbnailer.thumbnail(self.filename.path,(128,128), forceupdate=True)
-            self.thumbnailpath = thumbnaildata[0]
-            self.previewurl = thumbnaildata[1]
-            self.filetype = thumbnaildata[2]
+            thumbnaildata = thumbnail2(self.fileobject.filename.url ,(128,128))
+            self.filename = thumbnaildata[0]
+            self.filetype = thumbnaildata[1]
         except:
             self.filetype = "norender"
         super(fileobject, self).save()
+
     def delete(self, *args, **kwargs):
         super(fileobject, self).delete(*args, **kwargs)
         default_storage.delete(self.filename)
+        default_storage.delete(self.thumbname)
+
 
 class thumbobject(models.Model):
     #A pointer to the file this is a thumbnail of.
