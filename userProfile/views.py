6c363dd126dd7c17bfe2b58dcd761ec91ab6f657
Trist <tristantrim@gmail.com>
2013-09-14 16:55:34 -0300

    register is now the register and login page, and its niceish.

--- userProfile/views.py
+++ userProfile/views.py
@@ -111,39 +111,37 @@ def index(request, user):
 def register(request):
     
     ## If loop true when user clicks the register button.
+    from django.contrib.auth.forms import UserCreationForm
     if request.method == 'POST':
         #get data from the forms
-        form = registerForm(request.POST)
-        profileform = UserProfileForm(request.POST)
-        pictureform = UserPictureForm(request.POST, request.FILES)
-        ## puts the users stuff in the database if its valid.
-        if form.is_valid() and profileform.is_valid() and pictureform.is_valid():
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
             ###Create the user
             data = User();
             data.username = form.cleaned_data["username"]
-            data.password = form.cleaned_data["password"]###this is NOT how you set a password! Passwords are hashed.
-            data.set_password(form.cleaned_data["password"])
+           #data.password = form.cleaned_data["password"]###this is NOT how you set a password! Passwords are hashed.
+            data.set_password(form.cleaned_data["password1"])
             data.save()
             ###Create user's profile
-            profile = userProfile()
-            profile.user = data
-            profile.bio = profileform.cleaned_data["bio"]
-            #Create users picture.
-            profile.filename=request.FILES["filename"]
-            profile.save()
-            if profile.filename=="stoopid":
-                return render_to_response('register.html', dict( user=request.user, msg="Your profile pic didn't work, unsupported or something. Don't worry though, you can use the cool default one I drew if you want. btw, don't click the submit button again."))
+         #  profile = userProfile()
+         #  profile.user = data
+         #  profile.bio = profileform.cleaned_data["bio"]
+         #  #Create users picture.
+         #  try:
+         #      profile.filename=request.FILES["filename"]
+         #  except: "null"
+         #  profile.save()
+         #  if profile.filename=="stoopid":
+         #      return render_to_response('register.html', dict( user=request.user, msg="Your profile pic didn't work, unsupported or something. Don't worry though, you can use the cool default one I drew if you want. btw, don't click the submit button again."))
             return render_to_response('register.html', dict( user=request.user, msg="success. btw, don't click the submit button again."))
         #returns form with error messages.
         else:
-            return render_to_response('register.html', dict( user=request.user, form=form, form2=profileform, form3=pictureform))
+            return render_to_response('register.html', dict( user=request.user, form=form))
     
     ## Initializes the page with the forms.
     else:
-        form = registerForm()
-        profileform = UserProfileForm()
-        pictureform = UserPictureForm()
-        return render_to_response('register.html', dict( user=request.user, form=form, form2=profileform, form3=pictureform))
+        form = UserCreationForm()
+        return render_to_response('register.html', dict( user=request.user, form=form))
 
 ### simple logout view, redirects users to the login page.
 def logout_user(request):
