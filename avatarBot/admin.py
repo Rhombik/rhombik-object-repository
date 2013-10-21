aa2dd916a7f46a909f2e1c2b59ab5ec4d56281f0
traverseda <traverse.da@gmail.com>
2013-10-21 11:03:03 -0300

    Fixed dependency issue in avatarbor under 2.7

--- avatarBot/admin.py
+++ avatarBot/admin.py
@@ -1,7 +1,7 @@
 
 from post.models import *
 from django.contrib import admin
-from models import uploadPic, userPicThumb
+from avatarBot.models import uploadPic, userPicThumb
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ["title"]
