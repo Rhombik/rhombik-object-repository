ba0b23e0047bfda3605dc1b16f89d978b2f2786f
traverseda <traverse.da@gmail.com>
2014-03-13 06:04:56 -0300

    Added voting urls. project/PK/rate/[1,2] for up/down

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import *
 from django.conf import settings
+from djangoratings.views import AddRatingFromModel
+
 
 urlpatterns = patterns("",
+    #Controls the ratings
+    #/project/$PROJECTID/vote/1 for downvote. /project/$PROJECTID/vote/2 for upvote
+    url(r'project/(?P<object_id>\d+)/rate/(?P<score>\d+)/', AddRatingFromModel(), {
+        'app_label': 'project',
+        'model': 'project',
+        'field_name': 'rating',
+    }),
 
     (r'^project/(.*)/$', 'project.views.project'),
 
