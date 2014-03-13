1974dab1fd77e2066ddcdd16278db046d1c684dc
traverseda <traverse.da@gmail.com>
2014-03-13 07:11:53 -0300

    Made it easier to run different settings in production.

--- Settings/defaultSettings.py
+++ Settings/defaultSettings.py
@@ -127,10 +127,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'exampleSettings.urls'
+ROOT_URLCONF = 'Settings.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'exampleSettings.wsgi.application'
+WSGI_APPLICATION = 'Settings.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
---
+++ Settings/settings.py
@@ -0,0 +1,24 @@
+from Settings.defaultSettings import *
+
+
+##Create a unique secret key for the project
+try:
+    from Settings.secret_key import *
+except ImportError:
+    from django.utils.crypto import get_random_string
+    SETTINGS_DIR=os.path.abspath(os.path.dirname(__file__))
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    secret_key = get_random_string(50, chars)
+
+    secretfile = open(SETTINGS_DIR+"/secret_key.py", 'w')
+    secretfile.write("SECRET_KEY = \'"+secret_key+"\'\n")
+    from Settings.secret_key import *
+
+''''
+Production enviroment settings overrides. You can override Settings/defaultSettings.py settings from here.
+
+If you're a developer, you want to be editing the defaultSettings file instead.
+
+Only edit this file if you're deploying it. And make sure it's in your .gitignore.
+
+''''
