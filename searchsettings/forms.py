66f60ec815ff25dd8be4a0c1bc623bd9d9e07981
traverseda <traverse.da@gmail.com>
2014-03-26 01:36:49 -0300

    More search options.

--- searchsettings/forms.py
+++ searchsettings/forms.py
@@ -6,15 +6,26 @@ from taggit.utils import parse_tags
 from project.models import Project
 
 class DateRangeSearchForm(SearchForm):
-    start_date = forms.DateField(required=False)
-    end_date = forms.DateField(required=False)
+#    start_date = forms.DateField(required=False)
+#    end_date = forms.DateField(required=False)
     tags = forms.CharField(widget=TagAutoSuggest,required=False)
 
-    OPTIONS = (
+    sortOPTIONS = (
     ("false", "Sort by nothing"),
     ("false", "Also sort by noting"),
     )
-    sort = forms.ChoiceField(choices=OPTIONS)
+    sort = forms.ChoiceField(choices=sortOPTIONS)
+
+    timeOPTIONS = (
+    ("false", "all time"),
+    ("false", "this year"),
+    ("false", "this month"),
+    ("false", "this week"),
+    ("false", "today"),
+
+    )
+    From = forms.ChoiceField(choices=timeOPTIONS)
+
 
     def search(self):
         # First, store the SearchQuerySet received from other processing.
