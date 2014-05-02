83df84900cc565312ab239364f8164275514eae8
traverseda <traverse.da@gmail.com>
2014-05-02 20:10:02 -0300

    Tags exist, and more or less work.

--- taggit_autocomplete/urls.py
+++ taggit_autocomplete/urls.py
@@ -4,6 +4,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 from taggit_autocomplete.views import list_tags
 
 urlpatterns = patterns('',
-    url(r'^list/$', staff_member_required(list_tags),
+    url(r'^ajax/listtags/$', list_tags,
         name='taggit_autocomplete-list'),
 )
