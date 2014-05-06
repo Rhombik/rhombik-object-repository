e4cd1e01094c87480f52aecf7875f9d7e2f949b3
traverseda <traverse.da@gmail.com>
2014-05-06 03:18:35 -0300

    Well that was insane.

--- project/admin.py
+++ project/admin.py
@@ -1,9 +1,8 @@
 from project.models import *
 from django.contrib import admin
-
+from filemanager.admin import fileobjectAdmin
 
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ["title"]
-
 admin.site.register(Project, ProjectAdmin)
 
