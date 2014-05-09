cb196678654062e9d015f49478bf1affbc542115
Trist <tristantrim@gmail.com>
2014-05-09 11:01:47 -0300

    unbroke jsc3d viewer in the iframe on the project page.

--- exampleTheme/static/js/customScript.js
+++ exampleTheme/static/js/customScript.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
   	var image = $(this).attr('rel');
   	var height = $('.printableImageContainer').height();
   		$('.printableImageContainer').css('height', height);
-          	$('.printableImageContainer').html('<img src="' + image + '">');
+			// This is a bad if statement. Don't do this. Use better function.
+		if($(this).attr("class")=="imageSwitch iframe jsc3d"){
+          		$('.printableImageContainer').html('<iframe style="height:'+height+'px;width:100%;" src="' + image + '">');
+		}else{
+          		$('.printableImageContainer').html('<img src="' + image + '">');
+		}
            	$('.printableImageContainer').css('height', 'auto');
         e.preventDefault();
   });
