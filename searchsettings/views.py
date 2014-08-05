b499af212ece352713dfaaa2e873a79963525cf2
traverseda <traverse.da@gmail.com>
2014-08-05 05:25:30 -0300

    Search results now display decently.

--- searchsettings/views.py
+++ searchsettings/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
 
 from haystack.views import SearchView
-
+from project.views import project_list_get
 
 class SearchView(SearchView):
     def extra_context(self):
+        projects = []
+        for i in self.results:
+            projects += [i.object]
+            print(i.object)
+        data = project_list_get(projects)
         return {
-            'yourValue': "yourValue",
+            'listdata': data,
         }
